54e01ff87c42fa7d072cf953274bb9957a61b21b
Immanuel Almosara <ialmosara@gmail.com>
2019-12-22 21:09:11 -0500

    Successfully registers a new account to the database

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,13 +1,44 @@
-var express = require('express');
-var router = express.Router();
+//Imports
+const express = require('express');
+const cors = require('cors');
+const MongoClient = require('mongodb').MongoClient;
+
+const app = express();
+const router = express.Router();
+
+const corsOptions = {
+  origin: "http://localhost:3000"
+}
+
+MongoClient.connect('mongodb://localhost:27017/parasite-db', { useUnifiedTopology: true }, function (err, client) {
+  if (err) throw err;
+
+  const db = client.db('parasite-db')
+  
+  router.post('/', cors(corsOptions), (req, res) => {
+    // console.log(req, res);
+    db.collection('users').insertOne(req.body, function (err, result) {
+    if (err) throw err;
+    
+    console.log("User successfully inserted")
+  })
+    console.log(req.body);
+  });
+});
+
+app.options('http://localhost:3000/users', cors());
+
+router.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', cors(), function (req, res, next) {
   res.send('respond with a resource');
 });
 
-router.post('/', (req, res) => {
-  res.send('Post request successful');
-});
+
 
 module.exports = router;
--- src/components/Register.js
+++ src/components/Register.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Button from 'react-bootstrap/Button';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
 
 class Register extends Component {
 
@@ -10,60 +11,76 @@ class Register extends Component {
             firstName: "",
             lastName: "",
             email: "",
-            userName: "",
+            username: "",
             password: "",
-
+            confirmPassword: ""
         }
         this.registerAccount = this.registerAccount.bind(this);
+        this.changeHandler = this.changeHandler.bind(this);
     }
 
-    registerAccount(){
+    changeHandler = e => {
+        this.setState({[e.target.name]: e.target.value});
+    }
+
+    registerAccount = e => {
         //Should pull values once this is clicked
-        
+        e.preventDefault(); //Prevents page from refreshing
+        console.log(this.state);
+        axios.post('http://localhost:9000/users', this.state)
+            .then(function (response) {
+                const user = JSON.parse(response.config.data);
+                console.log(user);
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
     }
 
     render() {
         return (
-            <div className="container">
-                <div className="offset-md-3 col-6">
-                    <h2 className="text-center logo mb-3"> Register </h2>
+            <form onSubmit={this.registerAccount}>
+                <div className="container">
+                    <div className="col-12">
+                        <h2 className="text-center logo mb-3"> Register </h2>
 
-                    <p>First Name</p>
-                    <div className="input-group mb-3">
-                        <input type="text" className="form-control" placeholder="First Name" aria-label="First Name" aria-describedby="firstName" />
+                        <p>First Name</p>
+                        <div className="input-group mb-3">
+                            <input type="text" className="form-control" placeholder="First Name" name="firstName" onChange={this.changeHandler} />
+                        </div>
+
+                        <p>Last Name</p>
+                        <div className="input-group mb-3">
+                            <input type="text" className="form-control" placeholder="Last Name" name="lastName" onChange={this.changeHandler} />
+                        </div>
+
+                        <p>Email</p>
+                        <div className="input-group mb-3">
+                            <input type="text" className="form-control" placeholder="Email" name="email" onChange={this.changeHandler} />
+                        </div>
+
+                        <p>Username</p>
+                        <div className="input-group mb-3">
+                            <input type="text" className="form-control" placeholder="Username" name="username" onChange={this.changeHandler} />
+                        </div>
+
+                        <p>Password</p>
+                        <div className="input-group mb-3">
+                            <input type="text" className="form-control" placeholder="Password" name="password" onChange={this.changeHandler} />
+                        </div>
+
+                        <p>Confirm Password</p>
+                        <div className="input-group mb-3">
+                            <input type="text" className="form-control" placeholder="Confirm Password" name="confirmPassword" onChange={this.changeHandler} />
+                        </div>
+
+                        <Button type="submit" className="pr-3" onClick={this.registerAccount}>Register</Button>
+                        <Link to="/" className="ml-3">Already have an account?</Link>
+
                     </div>
-
-                    <p>Last Name</p>
-                    <div className="input-group mb-3">
-                        <input type="text" className="form-control" placeholder="Last Name" aria-label="Last Name" aria-describedby="lastName" />
-                    </div>
-
-                    <p>Email</p>
-                    <div className="input-group mb-3">
-                        <input type="text" className="form-control" placeholder="Email" aria-label="email" aria-describedby="email" />
-                    </div>
-
-                    <p>Username</p>
-                    <div className="input-group mb-3">
-                        <input type="text" className="form-control" placeholder="Username"/>
-                    </div>
-
-                    <p>Password</p>
-                    <div className="input-group mb-3">
-                        <input type="text" className="form-control" placeholder="Password" aria-label="Password" aria-describedby="password" />
-                    </div>
-
-                    <p>Confirm Password</p>
-                    <div className="input-group mb-3">
-                        <input type="text" className="form-control" placeholder="Confirm Password" aria-label="Confirm Password" aria-describedby="confirmPassword" />
-                    </div>
-
-                    <Button type="submit" onclick={this.registerAccount}>Register</Button>
-                    <Link to="/">Already have an account?</Link>
 
                 </div>
-
-            </div>
+            </form>
         )
     }
 }
