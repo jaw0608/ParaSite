e192f0dd12f5d2472258791e47705723b25d5ce1
manny <ialmosara@gmail.com>
2021-04-23 01:31:14 -0400

    Added dynamic creation of rows based on player size - now need to pass in player amount as argument

--- src/components/Game.js
+++ src/components/Game.js
@@ -4,14 +4,30 @@ import { Button, Container, Row, Col } from 'react-bootstrap';
 const Game = (players) => {
     const getRows = (players) => {
         // 4 players per row
-        const rows = players/4;
+        let playerEntries = [];
+        const rows = parseInt(players/4);
         const remainder = players%4;
-        for (let i = 0; i < players/4; i++) {
-            
+
+        console.log(rows, remainder);
+        for (let i = 0; i < rows; i++){
+            playerEntries.push([])
+            for (let j = 0; j < 4; j++) {
+                playerEntries[i].push(j)
+            }
         }
+
+        if (remainder > 0) {
+            playerEntries.push([]);
+            for (let i = 0; i < remainder; i++) {
+                playerEntries[playerEntries.length-1].push(i)
+            }
+        }
+
+        console.log(playerEntries);
+
         return (
             <Container>
-                {rows.map(row => <RowComponent players={row}/>)}
+                {playerEntries.map(row => <RowComponent players={row}/>)}
             </Container>
         )
     }
@@ -20,14 +36,7 @@ const Game = (players) => {
         <Container>
             <Row><h1 className='titleText'> Game </h1> </Row>
             <Row>
-                  <Container>
-                      <Row>
-                          <Col className='room'> re </Col>
-                          <Col className='room'> re </Col>
-                          <Col className='room'> re </Col>
-                          <Col className='room'> re </Col>
-                      </Row>
-                  </Container>
+                  {getRows(14)}
             </Row>
             <Row>
                 <Button>Attack</Button>
@@ -39,6 +48,8 @@ const Game = (players) => {
 }
 
 const RowComponent = ({players}) => {
+    console.log(players);
+
     return (
         <Row>
             {players.map(player => <Col className='room'>{player}</Col>)}
